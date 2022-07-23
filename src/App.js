ba3d19aedec5221f28f8596d31885f602fb0cf0f
Shubham Saurabh <shubhamsaurabh@outlook.com>
2022-07-24 01:56:35 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [data, setData] = useState(() => {
+		const savedTodos = localStorage.getItem("todos");
+		if (savedTodos) {
+			return JSON.parse(savedTodos);
+		} else
+			return {
+				toDoList: [],
+				toDo: "",
+				priority: "",
+			};
+	});
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(data));
+  }, [data]);
+	const updateData = (e) => {
+		setData({
+			...data,
+			[e.target.name]: e.target.value,
+		});
+	};
+	const handleChange = (e) => {
+		updateData(e);
+	};
+
+	const validate = () => {
+		if (data.toDo === "") {
+			alert("Please enter a to do item");
+			return false;
+		}
+		if (data.priority === "") {
+			alert("Please enter a priority");
+			return false;
+		}
+		return true;
+	};
+
+	const handleSubmit = (e) => {
+		if (!validate()) {
+			return;
+		}
+		const obj = {
+			todo: data.toDo,
+			priority: data.priority,
+			id: data.toDoList.length + 1,
+		};
+		const currentList = [...data.toDoList];
+		currentList.push(obj);
+		setData({
+			...data,
+			toDoList: currentList,
+			toDo: "",
+		});
+	};
+
+	const handleDelete = (id) => {
+		setData({
+			...data,
+			toDoList: data.toDoList.filter((item) => item.id !== id),
+		});
+	};
+
+	return (
+		<div className="App">
+			<div className="container">
+				<input
+					name="toDo"
+					value={data.toDo}
+					onChange={handleChange}
+					type="text"
+					id="todoInput"
+					placeholder="List down your pending works here"
+				/>
+				<div className="priority">
+					<div className="priority-label">Priority:</div>
+					<div className="priority-buttons">
+						<input id="high" type="radio" name="priority" value="High" onChange={handleChange} />
+						<label htmlFor="high">High</label>
+						<input id="medium" type="radio" name="priority" value="Medium" onChange={handleChange} />
+						<label htmlFor="medium">Medium</label>
+						<input id="low" type="radio" name="priority" value="Low" onChange={handleChange} />
+						<label htmlFor="low">Low</label>
+					</div>
+				</div>
+
+				<button onClick={handleSubmit} id="addTodo">
+					Add
+				</button>
+				<ul id="todoList">
+					{data.toDoList.map((el) => (
+						<li key={el.id}>
+							{el.todo} {el.priority}
+							<button onClick={() => handleDelete(el.id)} className="delete">
+								Delete
+							</button>
+						</li>
+					))}
+				</ul>
+			</div>
+		</div>
+	);
 }
 
 export default App;
